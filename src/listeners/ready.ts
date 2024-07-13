fb7cedf9ec632b47f3e19913bb478e17755e7683
TejasLamba2006 <tejas22feb@gmail.com>
2024-07-13 15:21:22 +0530

    feat: Add ready listener for automatic GitHub updates
    This commit adds a new ready listener to handle automatic updates from GitHub. The listener checks for updates by running the command "git pull" at regular intervals. If there is a new update available, it sends a notification to the specified channel and restarts the bot.

---
+++ src/listeners/ready.ts
@@ -0,0 +1,60 @@
+import { Listener } from "@sapphire/framework";
+import type { Client, TextChannel } from "discord.js";
+import { exec } from "child_process";
+export class ReadyListener extends Listener {
+  public constructor(
+    context: Listener.LoaderContext,
+    options: Listener.Options
+  ) {
+    super(context, {
+      ...options,
+      event: "ready",
+    });
+  }
+  public run(client: Client) {
+    if (client === null) return;
+    const { username, id } = client.user!;
+    this.container.logger.info(`Successfully logged in as ${username} (${id})`);
+    try {
+      setInterval(() => {
+        let error_log = "";
+        exec(`git pull`, (error, stdout) => {
+          let response = String(error || stdout);
+          if (!error) {
+            if (response.includes("Already up to date.")) {
+              return;
+            } else {
+              (
+                client.channels.cache.get(
+                  process.env.GITHUB_LOGS_CHANNEL_ID || ""
+                ) as TextChannel
+              ).send(
+                "**[AUTOMATIC]** \nNew update on GitHub. Pulling. \n\nLogs: \n```" +
+                  response +
+                  "```" +
+                  "\n\n\n**Restarting bot**"
+              );
+              setTimeout(() => {
+                process.exit();
+              }, 1000);
+            }
+          } else {
+            if (error_log === response) return;
+            (
+              client.channels.cache.get(
+                process.env.GITHUB_LOGS_CHANNEL_ID || ""
+              ) as TextChannel
+            ).send(
+              "**[AUTOMATIC]** \nError while pulling from GitHub. \n\nLogs: \n```" +
+                response +
+                "```"
+            );
+            error_log = response;
+          }
+        });
+      }, 30000);
+    } catch (e) {
+      client.logger.error(e);
+    }
+  }
+}
