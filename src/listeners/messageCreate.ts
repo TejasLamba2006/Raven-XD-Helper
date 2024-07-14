a47302db8380d7ef916d4732ddc58a69a17e8f31
TejasLamba2006 <tejas22feb@gmail.com>
2024-07-14 23:30:07 +0530

    feat: Add Raven XD profile parser
    Parses uploaded Raven XD profiles, extracts enabled modules and keybind information, and presents it in an embedded message. This allows users to easily share and review their Raven XD configurations within the Discord server.

--- src/listeners/messageCreate.ts
+++ src/listeners/messageCreate.ts
@@ -1,6 +1,22 @@
 import { Listener } from "@sapphire/framework";
-import type { Message } from "discord.js";
+import { EmbedBuilder, type Message } from "discord.js";
 import { autoResponses } from "..";
+import { request } from "undici";
+import * as keycode from "keycode";
+interface Module {
+  name: string;
+  prettyName: string;
+  enabled: boolean;
+  hidden: boolean;
+  keybind: number | string;
+  [key: string]: string | number | boolean;
+}
+
+interface Profile {
+  bName: string;
+  keybind: number;
+  modules: Module[];
+}
 
 export class MessageCreateListener extends Listener {
   public constructor(
@@ -15,6 +31,79 @@ export class MessageCreateListener extends Listener {
   public async run(message: Message<true>) {
     if (message.author?.bot) {
       return;
+    }
+    if (message.attachments.size > 0) {
+      const attachment = message.attachments.first();
+      console.log(attachment?.contentType);
+      if (attachment?.contentType === "application/json; charset=utf-8") {
+        const data = await request(attachment.url);
+        const json = JSON.parse(data.toString()) as Profile;
+        if (json) {
+          const reply = await message.reply({
+            content: "Received a Raven XD profile, processing...",
+          });
+          await message.react("👍");
+          const enabled = json.modules.filter((module) => module.enabled);
+          const keybinds = json.modules.filter(
+            (module) => module.keybind !== 0
+          );
+          const embed = new EmbedBuilder()
+            .setTitle(attachment.name.split(".")[0])
+            .setColor("Green")
+            .addFields(
+              {
+                name: "🟢Enabled Module",
+                value:
+                  enabled
+                    .map((module) =>
+                      Object.entries(module)
+                        .map(([key, value]) => {
+                          if (typeof value === "boolean") {
+                            value = value ? "Yes" : "No";
+                          }
+                          if (key === "keybind") {
+                            if (
+                              typeof value === "string" &&
+                              value.startsWith("M")
+                            ) {
+                              value = parseInt(value.split("M")[1]) + 1000;
+                            }
+                            value = keycode(value as number);
+                          }
+                          if (key === "prettyName") {
+                            return;
+                          }
+                          return `${key}: \`${value}\``;
+                        })
+                        .join("\n")
+                    )
+                    .join("\n") || "None",
+              },
+              {
+                name: "🔑Keybind",
+                value:
+                  keybinds
+                    ?.map((module) =>
+                      Object.entries(module)
+                        .map(([key, value]) => {
+                          if (key === "keybind") {
+                            if (
+                              typeof value === "string" &&
+                              value.startsWith("M")
+                            ) {
+                              value = parseInt(value.split("M")[1]) + 1000;
+                            }
+                            value = keycode(value as number);
+                          }
+                        })
+                        .join("\n")
+                    )
+                    .join("\n") || "None",
+              }
+            );
+          await reply.edit({ embeds: [embed], content: "" });
+        }
+      }
     }
     const prefix = (await this.container.client.fetchPrefix(message)) || "!";
     if (message.content.startsWith(prefix?.toString())) {
